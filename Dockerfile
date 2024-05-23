3c1517e4bce7e7509c85aa43007f1b422cc0f676
PastaGringo <pastagringo@protonmail.com>
2024-05-23 11:13:16 +0200

    nwc

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
 FROM golang:1.21.8-alpine3.19 AS builder
 RUN apk add git gcc musl-dev linux-headers
 WORKDIR /opt/buid
-RUN git clone https://github.com/braydonf/satdress.git /opt/buid
+RUN git clone --branch nwc https://github.com/braydonf/satdress.git /opt/buid
 RUN go get
 RUN go build
+RUN go build ./cli/satdress-cli.go
+
 FROM alpine:3.14
-COPY --from=builder /opt/buid/satdress /usr/local/bin/
-EXPOSE 17422
+# COPY --from=builder /opt/buid/satdress /usr/local/bin/
+COPY --from=builder /opt/buid/ /usr/local/bin/
+EXPOSE 8080
 COPY entrypoint.sh /
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 echo
-echo "------------"
-echo "  SATDRESS  "
-echo "------------"
-echo
-echo "Github repo: https://github.com/braydonf/satdress"
-echo "Satdress docker image by PastaGringo: https://github.com/PastaGringo/satdress-docker"
+echo "  _____  ____  ______  ___    ____     ___  _____ _____";
+echo " / ___/ /    ||      ||   \  |    \   /  _]/ ___// ___/";
+echo "(   \_ |  o  ||      ||    \ |  D  ) /  [_(   \_(   \_ ";
+echo " \__  ||     ||_|  |_||  D  ||    / |    _]\__  |\__  |";
+echo " /  \ ||  _  |  |  |  |     ||    \ |   [_ /  \ |/  \ |";
+echo " \    ||  |  |  |  |  |     ||  .  \|     |\    |\    |";
+echo "  \___||__|__|  |__|  |_____||__|\_||_____| \___| \___|";
+echo "                                                       ";
+echo "Satdress github repo                  : https://github.com/braydonf/satdress"
+echo "Satdress docker image by PastaGringo  : https://github.com/PastaGringo/satdress-docker"
 echo
 echo "---------------------------"
 echo "  Docker Compose Env Vars"
@@ -22,11 +26,15 @@ echo "SITE_NAME         : $SITE_NAME"
 echo "PHOENIXD_HOST     : $PHOENIXD_HOST"
 echo "PHOENIXD_PORT     : $PHOENIXD_PORT"
 echo "PHOENIXD_KEY      : $PHOENIXD_KEY"
-echo "USER_ADDRESS      : $USER_ADDRESS"
+echo "USER              : $USER"
+echo "NWC_RELAY         : $NWC_RELAY"
 echo
 echo "------------------------"
 echo "  Checking Satdress..."
 echo "------------------------"
+echo
+path_config="config.yml"
+echo "> Config.yml path   : $path_config" 
 echo
 if [ -f "docker_setup_done" ]; then
     echo ">>> Satdress has already been configured. Nothing to configure."
@@ -37,27 +45,64 @@ if [ -f "docker_setup_done" ]; then
     echo
     /usr/local/bin/satdress
 else
-    echo "> Satdress is started for the first time!"
+    echo ">>> Satdress is started for the first time!"
+    echo
+    echo "------------------------"
+    echo "  Generating NWC keys..."
+    echo "------------------------"
+    echo
+    /usr/local/bin/satdress-cli nwc keygen >> nwc.keys
+    private_key=$(grep "private key:" "nwc.keys" | cut -d ' ' -f 3)
+    private_key_hex=$(grep "public key:" "nwc.keys" | cut -d ' ' -f 3)
+    nsec=$(grep "nsec:" "nwc.keys" | cut -d ' ' -f 2)
+    npub=$(grep "npub:" "nwc.keys" | cut -d ' ' -f 2)
+    echo "Private key       : $private_key"
+    echo "Private key Hex   : $private_key_hex"
+    echo "Nsec              : $nsec"
+    echo "Npub              : $npub"
+    echo
     echo "> Configuring Satdress..."
     echo "- Creating config.yml..."
-    echo "host: $HOST" >> config.yml
-    echo "port: $PORT" >> config.yml
-    echo "domain: $DOMAIN" >> config.yml
-    echo "siteownername: $SITE_OWNER_NAME" >> config.yml
-    echo "siteownerurl: $SITE_OWNER_URL" >> config.yml
-    echo "sitename: $SITE_NAME" >> config.yml
-    echo "nostrprivatekey: $NOSTRPRIVATEKEY" >> config.yml
-    echo ""  >> config.yml
-    echo "users:" >> config.yml
-    echo "  - name: $USER_ADDRESS" >> config.yml
-    echo "    kind: phoenix" >> config.yml
-    echo "    host: $PHOENIXD_HOST:$PHOENIXD_PORT" >> config.yml
-    echo "    key: $PHOENIXD_KEY" >> config.yml
+    echo "host: $HOST" >> $path_config
+    echo "port: $PORT" >> $path_config
+    echo "domain: $DOMAIN" >> $path_config
+    echo "siteownername: $SITE_OWNER_NAME" >> $path_config
+    echo "siteownerurl: $SITE_OWNER_URL" >> $path_config
+    echo "sitename: $SITE_NAME" >> $path_config
+    echo "nostrprivatekey: $NOSTRPRIVATEKEY" >> $path_config
+    echo ""  >> $path_config
+    echo "users:" >> $path_config
+    echo "  - name: $USER" >> $path_config
+    echo "    kind: phoenix" >> $path_config
+    echo "    host: $PHOENIXD_HOST:$PHOENIXD_PORT" >> $path_config
+    echo "    key: $PHOENIXD_KEY" >> $path_config
+    echo "    nwcsecret: $private_key_hex" >> $path_config
+    echo "    nwcrelay: $NWC_RELAY" >> $path_config
+    echo
     echo "> Configuration is done ✅ "
     echo
-    echo "------------------------"
-    echo "  Starting Satdress..."
-    echo "------------------------"
+    echo "--------------------------"
+    echo "  Starting Satdress... ⚡"
+    echo "--------------------------"
     echo
-    /usr/local/bin/satdress
+    nohup /usr/local/bin/satdress > satdress.out &
+    sleep 10
+    echo
+    echo "-----------------------------"
+    echo "  Generating NWC QR Code..."
+    echo "-----------------------------"
+    echo
+    /usr/local/bin/satdress-cli nwc connect-qrcode --user $USER
+    echo
+    echo "------------------------------------"
+    echo "  Generating NWC Connect String..."
+    echo "------------------------------------"
+    echo
+    /usr/local/bin/satdress-cli nwc connect-string --user $USER
+    echo
+    echo "----------------------------------------------------"
+    echo "  Displaying Satdress log... (waiting new payment)"
+    echo "----------------------------------------------------"
+    echo
+    tail -f satdress.out
 fi
